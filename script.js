5a8827b6e4bb96998cda5ce148258092123d990e
Alvin Wan <hi@alvinwan.com>
2015-07-28 16:05:05 -0700

    added firebase capabilities

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-var firepadRef = new Firebase('wwworld.firebaseio.com');
-var codeMirror = CodeMirror($('.sync').get(), { lineWrapping: false });
-var firepad = Firepad.fromCodeMirror(firepadRef, codeMirror,
-      { richTextShortcuts: false, richTextToolbar: false, defaultText: 'Hello, World!' });
+$(document).ready(function() {
+
+	function init(selector) {
+		// New Firebase connection
+		var firepadRef = new Firebase('wwworld.firebaseio.com');
+		
+		// Create CodeMirror (with line numbers and the JavaScript mode).
+		var codeMirror = CodeMirror(document.getElementById(selector), {
+			lineNumbers: true,
+			mode: 'javascript'
+		});
+
+		// Create Firepad.
+		var firepad = Firepad.fromCodeMirror(firepadRef, codeMirror, {
+			defaultText: '// JavaScript Editing with Firepad!\nfunction go() {\n  var message = "Hello, world.";\n  console.log(message);\n}'
+		});
+	}
+
+	init('sync');
+});
